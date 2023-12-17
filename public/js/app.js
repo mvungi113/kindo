80a70b45f16a3885da242d313ea056c344a0f914
devsTz <rogashianmvungi@gmail.com>
2023-12-17 09:18:58 +0300

    KINDO project is under construction

---
+++ public/js/app.js
@@ -0,0 +1,7 @@
+
+// Get current date and time
+var now = new Date();
+var datetime = now.toLocaleString();
+
+// Insert date and time into HTML
+document.getElementById("datetime").innerHTML = datetime;
